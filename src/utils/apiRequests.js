ea3a2138f53da453dd93e93707d784df0b7feac2
Alejandro Castro <alejo199821@gmail.com>
2023-03-30 11:44:33 -0400

    complete front end file setup

---
+++ src/utils/apiRequests.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+//AXIOS UTIL FUNCTIONS
+const baseURL = process.env.REACT_APP_BASE_URL;
+
+//EXAMPLE AXIOS CALL
+
+// export async function getWarehouses() {
+//     try {
+//         const res = await axios.get(`${baseURL}/warehouses`)
+//         return res.data;
+//     } catch {
+//         return "Failed to request Warehouses";
+//     }
+// }
+
+
